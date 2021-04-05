c6336321f9a86833c668c54db34562adcf5f25e1
nothingexessive <liuxjames88@gmail.com>
2021-04-05 13:05:27 -0400

    Adding jquery to lab navigation page

---
+++ website/pages/projects.js
@@ -0,0 +1,7 @@
+$( function() {
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+  } );
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
